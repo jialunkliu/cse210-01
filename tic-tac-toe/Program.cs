d221d10c412e6eefcb7bdba9b2cc003b5e935cda
Jialun Liu <95363814+jialunkliu@users.noreply.github.com>
2022-04-30 14:18:58 -0600

    Add position list and main fuction

--- tic-tac-toe/Program.cs
+++ tic-tac-toe/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+using System.Collections.Generic;
+
+List<string> position = new List<string>()
+{"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+void main()
+{
+    Console.WriteLine("Hello, World!");
+}
+
+main();
